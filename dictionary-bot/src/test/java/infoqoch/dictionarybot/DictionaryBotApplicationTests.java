e492483ac85f9d85a6703f226b3c279614b6bc3c
bae <infoqoch@gmail.com>
2022-05-07 17:51:48 +0900

    init

---
+++ dictionary-bot/src/test/java/infoqoch/dictionarybot/DictionaryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package infoqoch.dictionarybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictionaryBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
