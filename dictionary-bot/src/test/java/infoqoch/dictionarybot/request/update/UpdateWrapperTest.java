1bd1a8c310d55245929ae30ccd4ced3b931fd02a
bae <infoqoch@gmail.com>
2022-06-01 15:35:28 +0900

    mock update를 외부 클래스에서 사용할 수 있도록 변경하였음.

--- dictionary-bot/src/test/java/infoqoch/dictionarybot/request/update/UpdateWrapperTest.java
+++ dictionary-bot/src/test/java/infoqoch/dictionarybot/request/update/UpdateWrapperTest.java
@@ -13,11 +13,13 @@ import java.util.List;
 
 import static infoqoch.dictionarybot.request.DictionaryCommand.HELP;
 import static infoqoch.dictionarybot.request.DictionaryCommand.LOOKUP_WORD;
+import static infoqoch.dictionarybot.request.update.MockUpdateJsonGenerate.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UpdateWrapperTest {
     JsonBind jsonBind = new DefaultJsonBind();
 
+    // MockUpdateJsonGenerate에 사용한 raw json이 아래의 데이터임.
     @Test
     void resolve_type() throws JsonProcessingException {
         String chat_json = "{\"ok\":true,\"result\":[{\"update_id\":567841804,\n" +
@@ -32,32 +34,8 @@ class UpdateWrapperTest {
     }
 
     private void assertType(String json, UpdateType type) {
-        UpdateWrapper wrapper = resolveType(json);
+        UpdateWrapper wrapper = MockUpdateJsonGenerate.resolveType(json);
         assertThat(wrapper.type()).isEqualTo(type);
-    }
-
-    private UpdateWrapper resolveType(String json) {
-        Response<List<Update>> response = jsonBind.toList(json, Update.class);
-        final Update update = response.getResult().get(0);
-        UpdateWrapper wrapper = new UpdateWrapper(update);
-        return wrapper;
-    }
-
-    private String mockChatJsonUpdate(String text) {
-        return mockChatJsonUpdate(text, 12354235l);
-    }
-
-    private String mockChatJsonUpdate(String text, long chatId) {
-        return "{\"ok\":true,\"result\":[{\"update_id\":567841804,\n" +
-                "\"message\":{\"message_id\":2102,\"from\":{\"id\":" + chatId + ",\"is_bot\":false,\"first_name\":\"\\uc11d\\uc9c4\",\"language_code\":\"ko\"},\"chat\":{\"id\":39327045,\"first_name\":\"\\uc11d\\uc9c4\",\"type\":\"private\"},\"date\":1652025791,\"text\":\"" + text + "\"}}]}";
-    }
-
-    private String mockDocumentJsonUpdate(String caption) {
-        return "{\"ok\": true,\"result\": [{\"update_id\": 567841807,\"message\": {\"message_id\": 2149,\"from\": {\"id\": 39327045,\"is_bot\": false,\"first_name\": \"\\uc11d\\uc9c4\",\"language_code\": \"ko\"},\"chat\": {\"id\": 39327045,\"first_name\": \"\\uc11d\\uc9c4\",\"type\": \"private\"},\"date\": 1653482399,\"document\": {\"file_name\": \"test.txt\",\"mime_type\": \"text/plain\",\"file_id\": \"BQACAgUAAxkBAAIIZWKOI5-9F9n8O1nh5Bz2m505K7qfAAI5BgACNKpwVL13NImKS4jvJAQ\",\"file_unique_id\": \"AgADOQYAAjSqcFQ\",\"file_size\": 8},\"caption\": \""+caption+"\"}}]}";
-    }
-
-    private String mockPhotoJsonUpdate(String caption) {
-        return "{\"ok\": true,\"result\": [{\"update_id\": 567841808,\"message\": {\"message_id\": 2150,\"from\": {\"id\": 39327045,\"is_bot\": false,\"first_name\": \"\\uc11d\\uc9c4\",\"language_code\": \"ko\"},\"chat\": {\"id\": 39327045,\"first_name\": \"\\uc11d\\uc9c4\",\"type\": \"private\"},\"date\": 1653482413,\"photo\": [{\"file_id\": \"AgACAgUAAxkBAAIIZmKOI6wEb4PQtzRFKkLv8fPtja6tAAJYsTEbNKpwVIdigkCEtD4HAQADAgADcwADJAQ\",\"file_unique_id\": \"AQADWLExGzSqcFR4\",\"file_size\": 1196,\"width\": 90,\"height\": 90},{\"file_id\": \"AgACAgUAAxkBAAIIZmKOI6wEb4PQtzRFKkLv8fPtja6tAAJYsTEbNKpwVIdigkCEtD4HAQADAgADbQADJAQ\",\"file_unique_id\": \"AQADWLExGzSqcFRy\",\"file_size\": 15474,\"width\": 320,\"height\": 320},{\"file_id\": \"AgACAgUAAxkBAAIIZmKOI6wEb4PQtzRFKkLv8fPtja6tAAJYsTEbNKpwVIdigkCEtD4HAQADAgADeAADJAQ\",\"file_unique_id\": \"AQADWLExGzSqcFR9\",\"file_size\": 52057,\"width\": 800,\"height\": 800},{\"file_id\": \"AgACAgUAAxkBAAIIZmKOI6wEb4PQtzRFKkLv8fPtja6tAAJYsTEbNKpwVIdigkCEtD4HAQADAgADeQADJAQ\",\"file_unique_id\": \"AQADWLExGzSqcFR-\",\"file_size\": 70180,\"width\": 1280,\"height\": 1280}],\"caption\": \""+caption+"\"}}]}";
     }
 
     @Test
