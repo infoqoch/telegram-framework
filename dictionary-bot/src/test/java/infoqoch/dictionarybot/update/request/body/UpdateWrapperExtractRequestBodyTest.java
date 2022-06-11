bade8cfda8c8c6f2b7c6e5fe40ae8bd6da07ef45
bae <infoqoch@gmail.com>
2022-06-11 09:31:17 +0900

    명칭 변경

--- dictionary-bot/src/test/java/infoqoch/dictionarybot/update/request/body/UpdateWrapperExtractRequestBodyTest.java
+++ dictionary-bot/src/test/java/infoqoch/dictionarybot/update/request/body/UpdateWrapperExtractRequestBodyTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class UpdateWrapperToRequestTest {
+public class UpdateWrapperExtractRequestBodyTest {
     @Test
     void wrapper_to_chat_request() {
         final Update update = MockUpdateJsonGenerate.extractUpdate(MockUpdateJsonGenerate.mockChatJsonUpdate("/help"));
