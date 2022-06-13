e2cc46c23fe84244e7ffa46418d09e59b832b484
bae <infoqoch@gmail.com>
2022-06-13 23:09:31 +0900

    시트 두 개 테스트

--- dictionary-bot/src/test/java/infoqoch/dictionarybot/util/excel/ExcelReadTest.java
+++ dictionary-bot/src/test/java/infoqoch/dictionarybot/util/excel/ExcelReadTest.java
@@ -9,11 +9,23 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ExcelReadTest {
     @Test
-    void read_excel(){
+    void read_single_sheet_excel(){
         File file = new File(getClass().getClassLoader().getResource("exceltest/test1.xlsx").getFile());
         final ExcelReader excelReader = new ExcelReader(file, 4);
         final List<List<List<String>>> extract = excelReader.extract();
         assertThat(extract.toString()).isEqualTo("[[[a, b, c, d], [1.0, , 3.0, ], [, , , ], [, , , end]]]");
+    }
+
+    @Test
+    void read_double_sheet_excel(){
+        File file = new File(getClass().getClassLoader().getResource("exceltest/test2.xlsx").getFile());
+        final ExcelReader excelReader = new ExcelReader(file, 4);
+        final List<List<List<String>>> extract = excelReader.extract();
+        assertThat(extract.toString()).isEqualTo("" +
+                "[" +
+                "[[a, b, c, d], [1.0, , 3.0, ], [, , , ], [, , , end]]" + // 시트 1
+                ", [[1.0, 2.0, 3.0, 4.0], [, , , ], [, , , ], [, 6.0, , ], [, , , 8.0]]" + // 시트 2
+                "]");
     }
 
 }
