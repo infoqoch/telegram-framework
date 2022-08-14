8bb862d4c0f5b6c3bdf24105a287082769ff1f78
bae <infoqoch@gmail.com>
2022-08-14 12:12:20 +0900

    기존에 있던 config로 옮김 ㅠ

--- dictionary-bot/src/main/java/infoqoch/dictionarybot/system/scheduler/SchedulerConfig.java
+++ dictionary-bot/src/main/java/infoqoch/dictionarybot/system/scheduler/SchedulerConfig.java
@@ -1,10 +1,14 @@
 package infoqoch.dictionarybot.system.scheduler;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+@Profile("scheduler")
+@EnableScheduling
 @Configuration
 public class SchedulerConfig implements SchedulingConfigurer {
 
